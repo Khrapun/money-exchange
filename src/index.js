12b210d241560218b46a0bc1bc03ea216232bc89
Khrapun <pavelhrapun@mail.ru>
2019-02-25 22:20:46 +0300

    feach: add function which accept amout of money and splits into coins

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency <= 0){
+        let bank = {};
+        return bank;
+    }
+    else if(currency > 10000){
+        let bank = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        return bank;
+    }
+    else{
+    let bank = {H:50, Q:25, D:10, N:5, P:1}
+    let balance = currency%bank.H;
+    bank.H = Math.floor(currency/bank.H);
+    
+    let balance2 = balance%bank.Q;
+    bank.Q = Math.floor(balance/bank.Q);
+    
+    let balance3 = balance2%bank.D;
+    bank.D = Math.floor(balance2/bank.D);
+    
+    let balance4 = balance3%bank.N;
+    bank.N = Math.floor(balance3/bank.N);
+    
+    let balance5 = balance4%bank.P;
+    bank.P = Math.floor(balance4/bank.P);
+    for(key in bank){
+        if (bank[key] == 0){
+            delete bank[key];
+        }
+    }
+    return bank;
+    }
 }
